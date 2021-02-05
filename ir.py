ff0afdad289a1bf900ee9ed1c1d0cee04cfadbac
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2021-02-05 20:26:54 +0800

    add try catch
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- ir.py
+++ ir.py
@@ -8,23 +8,26 @@ import os
 db = connect('qm9_ir_spectrum.db')
 
 for i in range(133885):
-    print('-' * 100, i)
-    os.system('ls | grep -v water.py | grep -v qm9.db| xargs rm')
-    a = read('qm9.db@' + str(i))
-    a.set_cell([10, 10, 10])
-    a.set_pbc([1, 1, 1])
-    
-    calc = Vasp(prec='Accurate',
-        ediff=1E-8,
-        isym=0,
-        idipol=4,       # calculate the total dipole moment
-        dipol=a.get_center_of_mass(scaled=True),
-        ldipol=True,
-        kpts=[1, 1, 1])
-    
-    a.calc = calc
-    ir = Infrared(a)
-    ir.run()
-    ir.summary()
-    db.write(a, data={'ir_spectrum':ir.get_spectrum()[0]})
-    # print(ir.get_spectrum()[0].shape)
+    try:
+        print('-' * 100, i)
+        os.system('ls | grep -v water.py | grep -v qm9*.db| xargs rm')
+        a = read('qm9.db@' + str(i))
+        a.set_cell([10, 10, 10])
+        a.set_pbc([1, 1, 1])
+        
+        calc = Vasp(prec='Accurate',
+            ediff=1E-8,
+            isym=0,
+            idipol=4,       # calculate the total dipole moment
+            dipol=a.get_center_of_mass(scaled=True),
+            ldipol=True,
+            kpts=[1, 1, 1])
+        
+        a.calc = calc
+        ir = Infrared(a)
+        ir.run()
+        ir.summary()
+        db.write(a, data={'ir_spectrum':ir.get_spectrum()[0]})
+        # print(ir.get_spectrum()[0].shape)
+    except:
+        pass
