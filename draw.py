80e322063191a3ccb01ed3df0364135ba127f6a6
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2021-02-25 08:58:27 +0800

    draw update
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- draw.py
+++ draw.py
@@ -1,14 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('data.txt')
+data_id = np.load('data_id.npy', allow_pickle=True).item()
 
-y1 = list(data[0]/np.amax(data[0]))
-y2 = list(data[1]/np.amax(data[1]))
-
-x = list(range(len(y1)))
-
-plt.plot(x, y2)
-plt.plot(x, y1)
-
-plt.show()
+for i in range(100):
+    data = np.loadtxt('data'+str(i)+'.txt')
+    
+    print(data_id[i+1])
+    y1 = list(data[0]/np.amax(data[0]))
+    y2 = list(data[1]/np.amax(data[1]))
+    
+    x = list(range(len(y1)))
+    
+    plt.plot(x, y2)
+    plt.plot(x, y1)
+    
+    plt.show()
