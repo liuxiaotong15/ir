1331ee590f1b44599716a903abee79da45b83788
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2021-04-06 21:59:13 +0800

    add pca in train
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- train.py
+++ train.py
@@ -5,7 +5,10 @@ import random, pickle
 import numpy as np
 from ase.formula import Formula
 
+from sklearn.decomposition import PCA
+# pca = PCA(n_components=600)
 
+# pca = None
 seed = 1234
 random.seed(seed)
 np.random.seed(seed)
@@ -29,7 +32,7 @@ with open('qm9_id_boc.lst', 'rb') as fp:
 
 print('pickle load finish...')
 
-extra_adding = 0 # due to the exception in ir.py
+# extra_adding = 0 # due to the exception in ir.py
 
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
@@ -52,7 +55,7 @@ def svm_penalty(var1, var2):
 
 class Model(nn.Module):
     def __init__(self, input_node, output_node):
-        print('model info:', input_node, output_node)
+        # print('model info:', input_node, output_node)
         node_cnt = 256
         super(Model, self).__init__()
         self.fc1 = nn.Linear(input_node, node_cnt)
@@ -75,7 +78,7 @@ class Model(nn.Module):
 
 def extract_descriptor(rows_input):
     global extra_adding
-    print('extract descriptor start')
+    # print('extract descriptor start')
     bocs, targets = [], []
     row_ids, syms = [], [] 
     id_sym_dict = {}
@@ -85,14 +88,15 @@ def extract_descriptor(rows_input):
         sym2 = 'B'
         while(Formula(str(sym1)) != Formula(str(sym2))):
             if str(sym1) != 'C' and str(sym2) != 'B':
-                print(row.id, sym1, sym2, 'An mismatch occur, extra id:', extra_adding)
+                # print(row.id, sym1, sym2, 'An mismatch occur, extra id:', extra_adding)
                 extra_adding += 1
             sym1 = row.toatoms().symbols
             sym2 = rows_qm9[row.id-1+extra_adding].toatoms().symbols
         if(str(sym1)=='CH4'):
             for i,j  in zip(row.data.ir_spectrum[0], row.data.ir_spectrum[1]):
                 if j>0.001:
-                    print(i, j)
+                    pass
+                    # print(i, j)
 
         row_ids.append(row.id)
         syms.append(str(sym1))
@@ -103,7 +107,10 @@ def extract_descriptor(rows_input):
         s = np.array(row.data.ir_spectrum[1])
         targets.append(s/np.amax(s))
 
-    #### shuffle ####
+    #### pca ####
+    global pca
+    targets = pca.fit_transform(targets)
+    #### shuffle together ####
     shfl = list(zip(bocs, targets, row_ids, syms))
     random.shuffle(shfl)
     bocs, targets, row_ids, syms = zip(*shfl)
@@ -115,76 +122,87 @@ def extract_descriptor(rows_input):
     return bocs, targets
 
 if __name__ == '__main__':
-    boc_lst_all, tgt_lst_all = extract_descriptor(rows_ir[:])
-    # training dataset
-    boc_lst, tgt_lst = boc_lst_all[:int(train_ratio * len(rows_ir))], tgt_lst_all[:int(train_ratio * len(rows_ir))]
-    # vali dataset 
-    vali_boc_lst, vali_tgt_lst = boc_lst_all[int(train_ratio * len(rows_ir)):int((train_ratio + vali_ratio) * len(rows_ir))], tgt_lst_all[int(train_ratio * len(rows_ir)):int((train_ratio + vali_ratio) * len(rows_ir))]
+    for comp in range(10, 1000, 10):
+        global pca, extra_adding
+        pca = PCA(n_components=comp)
+        extra_adding = 0
+        boc_lst_all, tgt_lst_all = extract_descriptor(rows_ir[:])
+        # training dataset
+        boc_lst, tgt_lst = boc_lst_all[:int(train_ratio * len(rows_ir))], tgt_lst_all[:int(train_ratio * len(rows_ir))]
+        # vali dataset 
+        vali_boc_lst, vali_tgt_lst = boc_lst_all[int(train_ratio * len(rows_ir)):int((train_ratio + vali_ratio) * len(rows_ir))], tgt_lst_all[int(train_ratio * len(rows_ir)):int((train_ratio + vali_ratio) * len(rows_ir))]
 
-    # train
-    model = Model(boc_lst[0].shape[0], tgt_lst[0].shape[0])
+        # train
+        model = Model(boc_lst[0].shape[0], tgt_lst[0].shape[0])
 
-    criterion = torch.nn.MSELoss() # Defined loss function
-    # criterion = torch.nn.CosineEmbeddingLoss(reduction='none') # Defined loss function
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4) # Defined optimizer
-    
-    x_data = torch.from_numpy(np.array(boc_lst)).to('cpu')
-    y_data = torch.from_numpy(np.array(tgt_lst)).to('cpu')
-    print('x, y shape:', x_data.shape, y_data.shape)
-    x_data_vali = torch.from_numpy(np.array(vali_boc_lst)).to('cpu')
-    y_data_vali = torch.from_numpy(np.array(vali_tgt_lst)).to('cpu')
+        criterion = torch.nn.MSELoss() # Defined loss function
+        # criterion = torch.nn.CosineEmbeddingLoss(reduction='none') # Defined loss function
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-4) # Defined optimizer
+        
+        x_data = torch.from_numpy(np.array(boc_lst)).to('cpu')
+        y_data = torch.from_numpy(np.array(tgt_lst)).to('cpu')
+        # print('x, y shape:', x_data.shape, y_data.shape)
+        x_data_vali = torch.from_numpy(np.array(vali_boc_lst)).to('cpu')
+        y_data_vali = torch.from_numpy(np.array(vali_tgt_lst)).to('cpu')
 
-    min_loss = 999
-    # Training: forward, loss, backward, step
-    # Training loop
-    for epoch in range(1000):
-        # Forward pass
+        min_loss = 999
+        # Training: forward, loss, backward, step
+        # Training loop
+        for epoch in range(1000):
+            # Forward pass
+            y_pred = model(x_data.double())
+        
+            # Compute loss
+            loss = criterion(y_pred, y_data) # + 1e-10 * l1_penalty(y_pred)
+            # loss = svm_penalty(y_pred, y_data) # + 1e-10 * l1_penalty(y_pred)
+            # loss = criterion(y_pred, y_data) - 1e-4 * torch.cosine_similarity(y_pred, y_data, dim=1).sum() # + 1e-10 * l1_penalty(y_pred)
+            # loss = criterion(y_pred, y_data).sum()
+
+            # Forward pass vali
+            y_pred_vali = model(x_data_vali.double())
+            # y_pred_vali = y_pred_vali/torch.max(y_pred_vali)
+
+            # Compute loss vali
+            loss_vali = criterion(y_pred_vali, y_data_vali)
+            # loss_vali = svm_penalty(y_pred_vali, y_data_vali)
+            # loss_vali = criterion(y_pred_vali, y_data_vali) - 1e-4 * torch.cosine_similarity(y_pred_vali, y_data_vali, dim=1).sum()
+
+            # print(epoch, loss.item(), loss_vali.item())
+
+            if loss_vali.item() < min_loss:
+                min_loss = loss_vali.item()
+                torch.save(model.state_dict(), 'best_model.dict')
+                # print('model saved')
+            # Zero gradients
+            optimizer.zero_grad()
+            # perform backward pass
+
+            loss.backward()
+            # update weights
+            optimizer.step()
+        model.load_state_dict(torch.load('best_model.dict'))
+        # inference dataset
+        moving = int((train_ratio+vali_ratio) * len(rows_ir))
+        boc_lst, tgt_lst = boc_lst_all[moving:], tgt_lst_all[moving:]
+        # inference
+        x_data = torch.from_numpy(np.array(boc_lst)).to('cpu')
+        y_data = torch.from_numpy(np.array(tgt_lst)).to('cpu')
         y_pred = model(x_data.double())
-    
-        # Compute loss
-        loss = criterion(y_pred, y_data) # + 1e-10 * l1_penalty(y_pred)
-        # loss = svm_penalty(y_pred, y_data) # + 1e-10 * l1_penalty(y_pred)
-        # loss = criterion(y_pred, y_data) - 1e-4 * torch.cosine_similarity(y_pred, y_data, dim=1).sum() # + 1e-10 * l1_penalty(y_pred)
-        # loss = criterion(y_pred, y_data).sum()
+        # print(y_data.shape, y_pred.shape)
 
-        # Forward pass vali
-        y_pred_vali = model(x_data_vali.double())
-        # y_pred_vali = y_pred_vali/torch.max(y_pred_vali)
-
-        # Compute loss vali
-        loss_vali = criterion(y_pred_vali, y_data_vali)
-        # loss_vali = svm_penalty(y_pred_vali, y_data_vali)
-        # loss_vali = criterion(y_pred_vali, y_data_vali) - 1e-4 * torch.cosine_similarity(y_pred_vali, y_data_vali, dim=1).sum()
-
-        print(epoch, loss.item(), loss_vali.item())
-
-        if loss_vali.item() < min_loss:
-            min_loss = loss_vali.item()
-            torch.save(model.state_dict(), 'best_model.dict')
-            print('model saved')
-        # Zero gradients
-        optimizer.zero_grad()
-        # perform backward pass
-
-        loss.backward()
-        # update weights
-        optimizer.step()
-    model.load_state_dict(torch.load('best_model.dict'))
-    # inference dataset
-    moving = int((train_ratio+vali_ratio) * len(rows_ir))
-    boc_lst, tgt_lst = boc_lst_all[moving:], tgt_lst_all[moving:]
-    # inference
-    x_data = torch.from_numpy(np.array(boc_lst)).to('cpu')
-    y_data = torch.from_numpy(np.array(tgt_lst)).to('cpu')
-
-    y_pred = model(x_data.double())
-    print(y_data.shape, y_pred.shape)
-    # check error
-    err_sum = 0
-    for i in range(y_data.shape[0]):
-        np.savetxt('data'+ str(i + moving) +'.txt', (y_data.detach().numpy()[i], y_pred.detach().numpy()[i]))
-        err_sum += abs(y_data.detach().numpy()[i].mean() - y_pred.detach().numpy()[i].mean())
-        print(y_data.shape[0], y_data.detach().numpy()[i].mean(), y_pred.detach().numpy()[i].mean(), 
-                np.amax(y_pred.detach().numpy()[i]), np.amax(y_data.detach().numpy()[i]))
-    print(err_sum/y_data.shape[0])
+        # inverse pca
+        inv_pca = np.matrix(pca.components_)
+        y_data = y_data.detach().numpy() * inv_pca + pca.mean_
+        y_pred = y_pred.detach().numpy() * inv_pca + pca.mean_
+        
+        # print(y_data.shape, y_pred.shape)
+        # check error
+        err_sum = 0
+        for i in range(y_data.shape[0]):
+            save_data = np.squeeze(np.array([y_data[i], y_pred[i]]))
+            np.savetxt('pic_data/data'+ str(i + moving) +'.txt', save_data)
+            err_sum += abs(y_data[i].mean() - y_pred[i].mean())
+            # print(y_data.shape[0], y_data[i].mean(), y_pred[i].mean(), 
+            #        np.amax(y_pred[i]), np.amax(y_data[i]))
+        print(comp, err_sum/y_data.shape[0])
 
